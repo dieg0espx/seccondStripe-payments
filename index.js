094058a097f8554d206bd8f9f473de852fce0c46
diegoTrueZenit <diego@truezenithcreative.com>
2023-06-14 15:38:10 -0600

    .

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import stripee from "stripe";
-const stripe = stripee("sk_test_51NJ0hELJsUTWMJlYVlH4BVzNUCsJZGwHpFSTgY764aso5LXqzJ4kCiQ76yAry1cmE3RX6rcuG8XpLop7NrY9O9KS005TRB27Nd");
+// const stripe = stripee("sk_test_51NJ0hELJsUTWMJlYVlH4BVzNUCsJZGwHpFSTgY764aso5LXqzJ4kCiQ76yAry1cmE3RX6rcuG8XpLop7NrY9O9KS005TRB27Nd");
 
 import bodyParser from 'body-parser';
 // const bodyParser = require("body-parser")
@@ -19,7 +19,18 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.post("/payment", cors(), async (req, res) => {
-	let { amount, id, description } = req.body
+	let { amount, id, description, propertyName } = req.body
+	
+	let stripe = stripee();
+	switch (propertyName) {
+		case 'Tuneberg':
+			stripe = stripee("sk_test_51NJ0hELJsUTWMJlYVlH4BVzNUCsJZGwHpFSTgY764aso5LXqzJ4kCiQ76yAry1cmE3RX6rcuG8XpLop7NrY9O9KS005TRB27Nd");
+			break;
+		default:
+			console.log("PROPERTY HAS NO OWNER");
+			break;
+	}
+
 	try {
 		const payment = await stripe.paymentIntents.create({
 			amount,
