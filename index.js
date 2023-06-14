7d78a0cde62f3fa6887eea05c75d9780542a3c02
diegoTrueZenit <diego@truezenithcreative.com>
2023-06-14 16:05:04 -0600

    .

--- index.js
+++ index.js
@@ -5,17 +5,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import stripee from "stripe";
-// const stripe = stripee("sk_test_51NJ0hELJsUTWMJlYVlH4BVzNUCsJZGwHpFSTgY764aso5LXqzJ4kCiQ76yAry1cmE3RX6rcuG8XpLop7NrY9O9KS005TRB27Nd");
-
 import bodyParser from 'body-parser';
-// const bodyParser = require("body-parser")
 import cors from "cors"
 
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-
-
 app.use(cors())
 
 app.post("/payment", cors(), async (req, res) => {
@@ -24,6 +19,9 @@ app.post("/payment", cors(), async (req, res) => {
 	let stripe = stripee();
 	switch (propertyName) {
 		case 'Tuneberg':
+			stripe = stripee("sk_test_51N0v1YCwOWyVBlDMWC6YFPjMvjlMtAPqlHvV0kFUNR969WYIJCWvAGRYP1cyWlazHYdcf7JhKBOzOeTUUklGSX9o00UW2HBQ9E");
+			break;
+		case 'Disabled':
 			stripe = stripee("sk_test_51NJ0hELJsUTWMJlYVlH4BVzNUCsJZGwHpFSTgY764aso5LXqzJ4kCiQ76yAry1cmE3RX6rcuG8XpLop7NrY9O9KS005TRB27Nd");
 			break;
 		default:
