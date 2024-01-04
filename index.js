b63b7acd511766930bf9d1820b44aac3fe4ecddc
diegoTrueZenit <diego@truezenithcreative.com>
2024-01-03 20:18:59 -0800

    .

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.post("/payment", cors(), async (req, res) => {
 })
 
 app.post("/paymentInflatables", cors(), async (req, res) => {
-	let { id, amount } = req.body
+	let { id, amount, description } = req.body
 
 	const Diego = process.env.DIEGO_SECRET_KEY;
 	let stripe = stripee(Diego);
